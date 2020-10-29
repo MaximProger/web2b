4b8e8f15a5d23a077a7e62e3270b827469e33992
Maksim Koshman <maksimkoshman090@gmail.com>
2020-10-29 21:07:02 +0300

    onlineoffice page is ready, only desktop

--- js/_custom.js
+++ js/_custom.js
@@ -8,4 +8,41 @@ $(document).ready(function () {
 
   // Маска для инпута (телефон)
   $("#phone").mask("9 999 999 99 99");
+
+  // Слайдер
+  $("#sliderFunctions").not(".slick-initialized").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: false,
+    asNavFor: "#navFunctions",
+  });
+
+  // Навигация для слайдера
+  $("#navFunctions").not(".slick-initialized").slick({
+    arrows: false,
+    slidesToShow: 4,
+    asNavFor: "#sliderFunctions",
+    focusOnSelect: true,
+  });
+
+  // Убирает маску у видео
+  $(".video__wrapper").click(function () {
+    $(".vedio__mask").hide();
+  });
+
+  // Табуляция в тарифах
+  $("#costBtn-1").click(function () {
+    $(".cost__top__btn").removeClass("active");
+    $(".cost__wrapper").removeClass("active");
+    $(this).addClass("active");
+    $("#costList-1").addClass("active");
+  });
+
+  $("#costBtn-2").click(function () {
+    $(".cost__top__btn").removeClass("active");
+    $(".cost__wrapper").removeClass("active");
+    $(this).addClass("active");
+    $("#costList-2").addClass("active");
+  });
 });
